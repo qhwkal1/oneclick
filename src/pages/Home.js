c4561d7f212a551af5ad616fce52d268078328d7
DH <lovein6114@nate.com>
2023-04-26 19:34:24 +0900

    테스트1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,10 @@
 import React from "react";
-import { Navigate } from "react-router-dom";
 
 const Home = () => {
 
   return (
     <>
-      <p>여기는 Home 입니다.</p>
+      <p>여기는 Home 입니다.1</p>
     </>
   );
 }
